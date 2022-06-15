6a4a4e8bac09ad33a3b158ef72cc2f375473f471
Satch2619 <ninho13@hotmail.de>
2022-06-15 23:11:27 +0200

    JavaScript Slide

---
+++ Slideshow/slideshow.js
@@ -0,0 +1,5 @@
+const indikatoren = document.getElementsByClassName('indikator');
+indikatoren[0].classList.add('aktiv');
+
+const slides = document.getElementsByClassName('slide');
+slides[0].classList.add('aktiv');
