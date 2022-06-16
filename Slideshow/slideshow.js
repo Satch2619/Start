8861990166ecc38f1c8305f921f76e0594bed1fe
Satch2619 <ninho13@hotmail.de>
2022-06-16 21:14:43 +0200

    Pfeil funktion erstellt JavaScript

--- Slideshow/slideshow.js
+++ Slideshow/slideshow.js
@@ -3,3 +3,25 @@ indikatoren[0].classList.add('aktiv');
 
 const slides = document.getElementsByClassName('slide');
 slides[0].classList.add('aktiv');
+
+var aktuellerIndex = 0;
+
+function umschalten(anzahl) {
+  indikatoren[aktuellerIndex].classList.remove('aktiv');
+  slides[aktuellerIndex].classList.remove('aktiv');
+
+  var neuerIndex = aktuellerIndex + anzahl;
+
+  if (neuerIndex < 0) {
+    neuerIndex = slides.length - 1;
+  }
+
+  if (neuerIndex > slides.length - 1) {
+    neuerIndex = 0;
+  }
+
+  indikatoren[neuerIndex].classList.add('aktiv');
+  slides[neuerIndex].classList.add('aktiv');
+
+  aktuellerIndex = neuerIndex;
+}
