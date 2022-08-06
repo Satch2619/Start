e2caf9588badfe1694ca298c6cf3f48880433828
Satch2619 <ninho13@hotmail.de>
2022-08-06 15:32:09 +0200

    Variable & Konstante

--- JavaScript Tutorial/app.js
+++ JavaScript Tutorial/app.js
@@ -1,7 +1,10 @@
 var playerName = "Sascha"; //string
-var alter = "40"; //number
+var alter = 40; //number
 var gameOver = false; //boolean
 var test; // undefined
 var nichts = null; // object
 
-console.log(playerName);
+const PI = 3.14;
+
+console.log(alter + 10);
+console.log(PI);
