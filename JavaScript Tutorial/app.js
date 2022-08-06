558340dc02a8f88bf5fae71eb0490be51c721c69
Satch2619 <ninho13@hotmail.de>
2022-08-06 15:42:37 +0200

    funtinen

--- JavaScript Tutorial/app.js
+++ JavaScript Tutorial/app.js
@@ -8,3 +8,18 @@ const PI = 3.14;
 
 console.log(alter + 10);
 console.log(PI);
+
+function calculate(number1, number2) {
+  let summe = number1 + number2;
+  return summe;
+}
+
+let a = 6;
+let b = 5;
+
+console.log(calculate(a, b));
+
+a = 7;
+b = 3;
+
+console.log(calculate(a, b));
