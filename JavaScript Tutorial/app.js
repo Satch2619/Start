6fb95f2d876ae40d0685af7b5043e54a45ee49e8
Satch2619 <ninho13@hotmail.de>
2022-08-06 15:51:42 +0200

    if und else

--- JavaScript Tutorial/app.js
+++ JavaScript Tutorial/app.js
@@ -1,25 +1,7 @@
-var playerName = "Sascha"; //string
-var alter = 40; //number
-var gameOver = false; //boolean
-var test; // undefined
-var nichts = null; // object
+let highscore = 100;
 
-const PI = 3.14;
-
-console.log(alter + 10);
-console.log(PI);
-
-function calculate(number1, number2) {
-  let summe = number1 + number2;
-  return summe;
+if (highscore > 99) {
+  console.log("Sascha ist der Beste");
+} else {
+  console.log("Das geht nooch besser!");
 }
-
-let a = 6;
-let b = 5;
-
-console.log(calculate(a, b));
-
-a = 7;
-b = 3;
-
-console.log(calculate(a, b));
