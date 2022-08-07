f9f2097a791139c569c1e0c1312c7d0e80a9bdc6
Satch2619 <ninho13@hotmail.de>
2022-08-07 13:08:08 +0200

    Event in/output

--- JavaScript Tutorial/app.js
+++ JavaScript Tutorial/app.js
@@ -2,5 +2,8 @@ let = clickButton = document.querySelector("#click-button");
 clickButton.addEventListener("click", eingabe);
 
 function eingabe() {
-  console.log("Hallo Button");
+  let input = document.querySelector("#input");
+  let output = document.querySelector("#output");
+
+  let newDiv = document.createElement("div");
 }
