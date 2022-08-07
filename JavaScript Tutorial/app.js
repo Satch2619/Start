64d3e7b8bbdfca3bddda98064f0c3bea47363f65
Satch2619 <ninho13@hotmail.de>
2022-08-07 11:56:51 +0200

    Schleifen

--- JavaScript Tutorial/app.js
+++ JavaScript Tutorial/app.js
@@ -1,20 +1,5 @@
-let obstListe = ["Banane", "Apfel", "Erdbeere"];
+let obstListe = ["Banane", "Apfel", "Erdbeere", "Kiwi"];
 
-// hinmzufügen an das Ende des Arrays
-obstListe.push("Heidelbeere");
-console.log(obstListe);
-
-// Entfernen des letzten Elements
-obstListe.pop();
-console.log(obstListe);
-
-// Hinzufügen am Anfang des Arrays
-obstListe.unshift("Kiwi");
-console.log(obstListe);
-
-// Entfernen des ersten Elements
-obstListe.shift();
-console.log(obstListe);
-
-obstListe.splice(1, 1);
-console.log(obstListe);
+obstListe.forEach(function (value) {
+  console.log(value);
+});
