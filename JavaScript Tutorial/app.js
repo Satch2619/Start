15801fb69d3a6e2d2112254b64b1144b4b0f6a73
Satch2619 <ninho13@hotmail.de>
2022-08-07 13:12:19 +0200

    Event Test erstellen

--- JavaScript Tutorial/app.js
+++ JavaScript Tutorial/app.js
@@ -6,4 +6,10 @@ function eingabe() {
   let output = document.querySelector("#output");
 
   let newDiv = document.createElement("div");
+  let newcontent = document.createTextNode(input.value);
+
+  newDiv.appendChild(newcontent);
+  output.appendChild(newDiv);
+
+  input.value = "";
 }
