e00011cc4ccfce6e6e8948898e46173bbff23408
Satch2619 <ninho13@hotmail.de>
2022-08-08 20:53:55 +0200

    console.Log

---
+++ JavaScript Tutorial/Console.Log/app.js
@@ -0,0 +1,14 @@
+let string = "Hallo Welt";
+let number = 123;
+let array = ["Eins", "Zwei", "Drei"];
+let object = {
+  name: "Max",
+  age: 26,
+  cat: { name: "Felix" },
+  getAge() {
+    return this.age;
+  },
+};
+let data = [];
+
+console.log(object);
