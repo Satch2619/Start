b207b44f52fae268978e69ac781e2352d518f21c
Satch2619 <ninho13@hotmail.de>
2022-08-19 22:15:56 +0200

    java erstellt

---
+++ Website erstellen mit Mario/js/app.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("theme") == "invert") {
+    document.querySelector("body").classList.add("invert");
+  }
+});
+
+document.querySelector("#invertButton").addEventListener("click", () => {
+  document.querySelector("body").classList.toggle("invert");
+
+  if (localStorage.getItem("theme") == "invert") {
+    localStorage.setItem("theme", "");
+  } else {
+    localStorage.setItem("theme", "invert");
+  }
+});
