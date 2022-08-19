36a1dba22f9ff141a8c64dc5f3b2feac623f219f
Satch2619 <ninho13@hotmail.de>
2022-08-19 22:36:38 +0200

    footer fertig

--- Website erstellen mit Mario/js/app.js
+++ Website erstellen mit Mario/js/app.js
@@ -2,6 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
   if (localStorage.getItem("theme") == "invert") {
     document.querySelector("body").classList.add("invert");
   }
+
+  let currenTime = new Date();
+  let year = currenTime.getFullYear();
+  document.querySelector("#currentYear").innerHTML = year;
 });
 
 document.querySelector("#invertButton").addEventListener("click", () => {
