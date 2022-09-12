990adb9d568c69a6e7565d0b5608c5dd497fb3da
Satch2619 <ninho13@hotmail.de>
2022-09-12 21:59:43 +0200

    modules

---
+++ JavaScript Tutorial/Modulare Programmierung/app.js
@@ -0,0 +1,19 @@
+let = persons = [
+  {
+    id: 0,
+    name: "Max",
+    hobby: "Pizza essen"
+  },
+  {
+    id: 1,
+    name: "Bodo",
+    hobby: "Eislaufen"
+  },
+  {
+    id: 2,
+    name: "Felix",
+    hobby: "Videospiele"
+  },
+]
+
+console.log(persons)
