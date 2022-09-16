3dafb84bb35dc3c1c8a145c05991eb5a3ae2c25f
Satch2619 <ninho13@hotmail.de>
2022-09-16 20:52:43 +0200

    constructor erstellt

--- JavaScript Tutorial/Objekt Orientierte Programmierung/app.js
+++ JavaScript Tutorial/Objekt Orientierte Programmierung/app.js
@@ -1,3 +1,12 @@
 class Mensch {
-  
+  constructor (vorname, nachname ) {
+    this.vorname = vorname;
+    this.nachname = nachname;
+  }
+  sagHallo() {
+    console.log("Hallo, mein Name ist "  + this.vorname +" "+ this.nachname);
+  }
 }
+
+let mensch1 = new Mensch("Sascha", "Holzheimer");
+mensch1.sagHallo();
