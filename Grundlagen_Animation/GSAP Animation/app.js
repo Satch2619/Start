8e127a669a1cf5262f477866f5b9ba32a60cf40f
Satch2619 <ninho13@hotmail.de>
2022-09-17 22:49:22 +0200

    animation vers01

--- Grundlagen_Animation/GSAP Animation/app.js
+++ Grundlagen_Animation/GSAP Animation/app.js
@@ -1,1 +1,5 @@
-gsap.from("#navigation li", {duration: 0.5, y: -20, opacity: 0, stagger: 0.2});
+
+gsap.to("#box1", { yPercent: -100});
+gsap.to("#box2", { yPercent: 100});
+
+gsap.from("#navigation li", {delay: 1, duration: 0.5, y: -20, opacity: 0, stagger: 0.2});
