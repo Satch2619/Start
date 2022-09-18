ef62b52c9e629937cc595c537995f565146d51bf
Satch2619 <ninho13@hotmail.de>
2022-09-18 21:40:29 +0200

    Animation fertig

--- Grundlagen_Animation/GSAP Animation/app.js
+++ Grundlagen_Animation/GSAP Animation/app.js
@@ -1,5 +1,15 @@
+gsap.defaults({ duration: 1.5, })
 
-gsap.to("#box1", { yPercent: -100});
-gsap.to("#box2", { yPercent: 100});
+let timeline = gsap.timeline();
 
-gsap.from("#navigation li", {delay: 1, duration: 0.5, y: -20, opacity: 0, stagger: 0.2});
+timeline.to("#box1", {yPercent: -100},0);
+timeline.to("#box2", {yPercent: 100},0);
+
+
+timeline.from("#teaserInfo", {duration:0.5, y:100, opacity:0});
+timeline.to("#teaserTitle", { y: -20, ease: "power4.out" , yoyo: true, yoyoEase: "bounce", repeat: 1, });
+
+timeline.from("#navigation li", {delay: 1, duration: 0.5, y: -20, opacity: 0, stagger: 0.2});
+
+
+
