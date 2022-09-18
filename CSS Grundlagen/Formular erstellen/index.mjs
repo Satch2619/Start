6d670e990613348e75cd2b6771a8196db476156c
Satch2619 <ninho13@hotmail.de>
2022-09-18 17:24:39 +0200

    mezhode erstellt

--- CSS Grundlagen/Formular erstellen/index.mjs
+++ CSS Grundlagen/Formular erstellen/index.mjs
@@ -1,9 +1,17 @@
 import express from 'express';
+import path from 'path';
 
 const app = express()
+const port = 3000
 
 app.get('/', function (req, res) {
-  res.send('Sascha lernt Web Development')
+  res.sendFile(path.resolve("seiten/index.html"))
 })
 
-app.listen(3000)
+app.post('/geheime-seite.html', function (req, res) {
+  res.sendFile(path.resolve("seiten/geheime-seite.html"))
+})
+
+app.listen(port,() => {
+  console.log(`Sascha ist der Beste ${port}`)
+})
