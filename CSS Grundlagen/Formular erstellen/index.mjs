c4142da629205999f0459306d7177b4a6f431100
Satch2619 <ninho13@hotmail.de>
2022-09-18 18:01:51 +0200

    login fertig

--- CSS Grundlagen/Formular erstellen/index.mjs
+++ CSS Grundlagen/Formular erstellen/index.mjs
@@ -4,11 +4,19 @@ import path from 'path';
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded())
+
 app.get('/', function (req, res) {
   res.sendFile(path.resolve("seiten/index.html"))
 })
 
-app.post('/geheime-seite.html', function (req, res) {
+app.post('/geheime-seite.html', (req, res) => {
+    const username = req.body.username;
+    const useremail = req.body.useremail;
+    const userpassword = req.body.userpassword;
+    if (username === 'Satch' && userpassword === 'Production') {
+  return res.sendFile(path.resolve("seiten/geheime-seite.html"))
+    }
   res.sendFile(path.resolve("seiten/geheime-seite.html"))
 })
 
