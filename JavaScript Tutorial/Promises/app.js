bab793bb53e261a1f237097a52915e21e32ee4e5
Satch2619 <ninho13@hotmail.de>
2022-09-20 19:34:26 +0200

    code auskommentiert

--- JavaScript Tutorial/Promises/app.js
+++ JavaScript Tutorial/Promises/app.js
@@ -1,17 +1,27 @@
-function getProducts(callback){
-  setTimeout(() => {
-    callback([
-        {name:"Milch", inStock: 5},
-        {name:"Butter", inStock: 8},
-      ]);
-  }, 1000);
-}
+let success = true;
 
-function findProduct(productName, callback) {
-  getProducts((products) => {
-    let product = products.find((product) => product.name === productName);
-    callback(product);
+function getProducts(){
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if(success) {
+        resolve([
+          {name:"Milch", inStock: 5},
+          {name:"Butter", inStock: 8},
+        ])
+      }
+      else{
+        reject("Fehler");
+      }
+    }, 1000);
   });
 }
 
-findProduct("Butter", console.log);
+
+//function findProduct(productName, callback) {
+//  getProducts((products) => {
+//    let product = products.find((product) => product.name === productName);
+//    callback(product);
+// });
+//}
+
+//findProduct("Butter", console.log);
