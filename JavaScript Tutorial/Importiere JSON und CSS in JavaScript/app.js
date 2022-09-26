7836322482b64e0e003c85a7d2361b99b5788946
Satch2619 <ninho13@hotmail.de>
2022-09-26 20:30:05 +0200

    loadModule

--- JavaScript Tutorial/Importiere JSON und CSS in JavaScript/app.js
+++ JavaScript Tutorial/Importiere JSON und CSS in JavaScript/app.js
@@ -1,3 +1,8 @@
-import { toConsole } from "./helperFunctions.js";
+//import { toConsole } from "./helperFunctions.js";
+//toConsole("Hallo Welt");
 
-toConsole("Hallo Welt");
+document.querySelector("#loadModule").addEventListener('click', function() {
+  import("./helperFunctions.js").then(({ toConsole }) => {
+    toConsole("dynamisches Hallo Welt");
+  });
+});
